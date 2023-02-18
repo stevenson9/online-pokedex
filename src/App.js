0e5a7d5016ad78725a6882699c7cb2490f20f832
stevenson9 <sdson9@gmail.com>
2023-02-18 00:05:23 -0800

    semi-reset, successfully printed out all pokemon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 
 import './App.css';
 import PokeBlock from './PokeBlock';
+import mockData from './mockData';
+import axios from 'axios';
 
 const API_URL = "https://pokeapi.co/api/v2/"
 
@@ -12,63 +14,62 @@ const charizard = {
 
 const App = () => {
 
-    const [pokemon, setPokemon] = useState([]);
-    const [lookUpPokemon, setLookUpPokemon] = useState("");
-
-    const searchPokemon = async (name) => {
-        const response = await fetch(`${API_URL}pokemon/${name}`);
-        const data = await response.json();
-
-        setPokemon(data);
-    }
+    const [pokemonData, setPokemonData] = useState({});
 
     useEffect(() => {
-        searchPokemon('')
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon?limit=1008`)
+            .then(function (response) {
+                const { data } = response;
+                const { results } = data;
+                const newPokemonData = {};
+                results.forEach((pokemon, index) => {
+                    newPokemonData[index + 1] = {
+                        id: index + 1,
+                        name: pokemon.name,
+                        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+                    };
+                });
+
+                setPokemonData(newPokemonData);
+
+
+            });
+
+    
     }, []);
 
 
+    const makePokemon = (pokemonId) => {
+        const { id, name, sprite } = pokemonData[pokemonId];
+        
+        return <PokeBlock pokemonId={id} name={name} key={pokemonId}/>
+    }
 
-    // when calling use "pokemon.{sprites}.{front_default}"
     return (
+
+        
         <div className="app">
             <h1>Pokedex</h1>
 
             <div className="searchbar">
                 <input
-                    placeholder="Search for a pokemon!"
-                    value={lookUpPokemon}
-                    onChange={(e) => {
-                        searchPokemon(lookUpPokemon);
-                        setLookUpPokemon(e.target.value);
-
-                    }}
+                    placeholder="Search for a pokemon!"           
                 />
             </div>
 
             <div className="container">
-                    
-                <PokeBlock pokemon={pokemon} />
+
+                <div className="pokemons">
+                {Object.keys(pokemonData).map((pokemonId) => 
+                makePokemon(pokemonId)
+                )}
+                </div>     
+                
             </div>
 
         </div>
     )
 }
-
-// const App = () => {
-
-//     return (
-//         <div className='app'>
-
-//             <h1>Pokedex</h1>
-
-//             <div className="searchbar">
-//                 <input
-//                     placeholder="Search for a pokemon!" />
-//             </div>
-//             <div className="container">
-//             </div>
-//         </div>
-//         )
-// }
 
 export default App;
