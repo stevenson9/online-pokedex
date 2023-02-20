0664b91a6de3c7d99ef0a2b7cad1fb1e35ee92bb
stevenson9 <sdson9@gmail.com>
2023-02-19 22:50:01 -0800

    able to search by pokedex number

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
 
     const [pokemonData, setPokemonData] = useState({});
     const [filter, setFilter] = useState("");
+    const [searchNum, setSearchNum] = useState(1);
     let [num, setNum] = useState(0);
     const [shiny, setShiny] = useState(false);
 
@@ -63,7 +64,7 @@ const App = () => {
                 <input
                     placeholder="Search for a pokemon!"
                     value={filter}
-                    onChange={(e) => { setFilter(e.target.value); setNum(0) }}
+                    onChange={(e) => { setFilter(e.target.value); setSearchNum(e.target.value); setNum(0) }}
                 />
 
             
@@ -75,7 +76,9 @@ const App = () => {
 
         
                     {Object.keys(pokemonData).map((pokemonId) =>
-                        pokemonData[pokemonId].name.includes(filter) && 
+                        (pokemonData[pokemonId].name.includes(filter) || 
+                        (pokemonData[pokemonId].id+'').includes(searchNum))
+                         && 
                         makePokemon(pokemonId)
                     )}
 
